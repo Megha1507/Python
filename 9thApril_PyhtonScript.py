16ef5bd3cc567825f70d0ed007942c6953bf7d94
Megha <meghs15sh@gmail.com>
2018-04-09 16:44:01 +0530

    1stScript_9April2018

---
+++ 9thApril_PyhtonScript.py
@@ -0,0 +1,17 @@
+# Printing a simple text. Can be writtenn single quotes also
+print("First Hello world program")
+
+# Concatenation of 2 numbers
+print("\nConcatenation of two numbers : ", "5"+"6")
+
+# Adding 2 numbers
+c = 5+7
+print("\nAdding two numbers: ", c)
+
+# Second method of adding 2 numbers
+print("\nSecond method of adding two numbers : ", int("5") + int("7"))
+
+print("\nPrinting strings in double quotes : \n", 'She said,"Don\'t do that"')
+
+print("\nConcatenating two STRINGS and a Number using str: ", "This is my " + str("1") + "st Program")
+
